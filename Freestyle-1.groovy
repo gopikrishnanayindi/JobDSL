787219a7cf5e5d7e0b1e5234d575ca21d12ca752
gopi <gopi.ramapuram@gmail.com>
2025-04-02 16:19:13 +0530

    job dsl adde

---
+++ Freestyle-1.groovy
@@ -0,0 +1,6 @@
+job('Freestyle-Job-1') {
+    description('This is a Freestyle Job')
+    steps {
+        shell('echo "Executing Freestyle Job 1"')
+    }
+}
---
+++ Freestyle-2.groovy
@@ -0,0 +1,6 @@
+job('Freestyle-Job-2') {
+    description('This is another Freestyle Job')
+    steps {
+        shell('echo "Executing Freestyle Job 2"')
+    }
+}
