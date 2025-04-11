826de6db76133cc0565b4a74967a04d6b44f3c73
Gopikrishna <gopi.ramapuram@gmail.com>
2025-04-11 06:31:52 +0530

    Create new_Pipeline.groovy

---
+++ DSLS/app2/new_Pipeline.groovy
@@ -0,0 +1,34 @@
+pipelineJob('New_Pipeline-Job') {
+    description('This is a Pipeline Job')
+    
+    definition {
+        cps {
+            script("""
+                pipeline {
+                    agent any
+                    
+                    stages {
+
+                        stage('Builds') {
+                            steps {
+                                echo 'Building...'
+                            }
+                        }
+                        
+                        stage('Test') {
+                            steps {
+                                echo 'Testing...'
+                            }
+                        }
+                        
+                        stage('Deploy') {
+                            steps {
+                                echo 'Deploying...'
+                            }
+                        }
+                    }
+                }
+            """)
+        }
+    }
+}
